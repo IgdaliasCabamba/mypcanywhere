b3211bb79c534373fc01c0bd4a726c6d98335a53
Igdálias Cabamba <igdaliasmussulveiracabamba@gmail.com>
2022-08-16 16:18:18 +0100

    added navbar

---
+++ app/src/lib/header.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import {
+        Collapse,
+        Navbar,
+        NavbarToggler,
+        NavbarBrand,
+        Nav,
+        Icon,
+        NavItem,
+        NavLink,
+        Dropdown,
+        DropdownToggle,
+        DropdownMenu,
+        DropdownItem,
+    } from "sveltestrap";
+    import Fa from 'svelte-fa/src/fa.svelte'
+    import { faDesktop } from '@fortawesome/free-solid-svg-icons/index.es'
+
+    let isOpen = false;
+
+    function handleUpdate(event) {
+        isOpen = event.detail.isOpen;
+    }
+
+</script>
+
+<Navbar color="dark" dark expand="md" fixed="top">
+    <NavbarBrand href="/">
+        <Fa icon={faDesktop} />
+    </NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+        <Nav class="ms-auto" navbar>
+            <NavItem>
+                <NavLink href="#">Home</NavLink>
+            </NavItem>
+            <NavItem>
+                <NavLink href="#"
+                    >Status</NavLink
+                >
+            </NavItem>
+            <Dropdown nav inNavbar>
+                <DropdownToggle nav caret>Help</DropdownToggle>
+                <DropdownMenu end dark>
+                    <DropdownItem>Github</DropdownItem>
+                    <DropdownItem>Discord</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>About</DropdownItem>
+                </DropdownMenu>
+            </Dropdown>
+        </Nav>
+    </Collapse>
+</Navbar>
--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,8 @@
 import './app.css'
 import App from './App.svelte'
+import "flex-splitter-directive/styles.min.css";
+import "flex-splitter-directive";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = new App({
   target: document.getElementById('app')
